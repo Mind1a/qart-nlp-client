40f3b2c75fb057777066763715f2e17b4bd7d31b
DavitDarjania <151033720+DavitDarjania@users.noreply.github.com>
2025-07-22 23:41:55 +0400

    feature/aboutPage
    - added desktop and mobile version of about page
    - who worked on project cards are fetching from db/json file
    - on text area added validations on input
    - in counting whitespaces tabs and common punctuations are not included
    - submit is impossible over 250 letters

---
+++ js/script.js
@@ -0,0 +1,49 @@
+// About page Start
+const articlesBox = document.querySelector(".articles_box");
+fetch("../db/worked_project.json")
+  .then((response) => response.json())
+  .then((data) => {
+    data.forEach((element) => {
+      const onProjectArticle = document.createElement("article");
+      onProjectArticle.classList.add("on_project_article");
+      const articleTitle = document.createElement("h3");
+      articleTitle.classList.add("article_title");
+      articleTitle.textContent = element.name;
+      const articleSubTitle = document.createElement("h4");
+      articleSubTitle.classList.add("article_subtitle");
+      articleSubTitle.textContent = element.position;
+      const articleIcon = document.createElement("aside");
+      articleIcon.classList.add("article_icon");
+      element.media.forEach((urll) => {
+        const img = document.createElement("img");
+        img.src = urll;
+        img.alt = element.name;
+        articleIcon.appendChild(img);
+      });
+      onProjectArticle.appendChild(articleTitle);
+      onProjectArticle.appendChild(articleSubTitle);
+      onProjectArticle.appendChild(articleIcon);
+      articlesBox.appendChild(onProjectArticle);
+    });
+  });
+
+const textArea = document.querySelector("#text_input");
+const wordCount = document.querySelector("#word_count");
+textArea.addEventListener("input", (e) => {
+  const str = textArea.value;
+  const matches = str.match(/[^ \t\n\r,.!?;:()"'`[\]{}<>\\/|@#$%^&*-+=~]/g);
+  if (matches) {
+    wordCount.textContent = matches.length + "/250";
+  } else {
+    wordCount.textContent = "0/250";
+  }
+  if (matches.length > 250) {
+    wordCount.style.color = "red";
+    // textArea.style.borderColor = "red";
+    textArea.setCustomValidity("Must be less than 250 words");
+  } else {
+    wordCount.style.color = "";
+    // textArea.style.borderColor = "";
+    textArea.setCustomValidity("");
+  }
+});
